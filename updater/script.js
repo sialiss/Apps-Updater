dbcdfa0dae559c78b77e5269d0f1a5f4462fa783
och <och.hco.och.hco@gmail.com>
2024-04-02 12:44:08 +0300

    hide update buttons when app not chosen

--- updater/script.js
+++ updater/script.js
@@ -4,7 +4,7 @@ async function info(appId) {
     }
     catch(error) {
         console.error(error)
-        return "Choose an app!"
+        return "Info unavailable!"
     }
 }
 
@@ -41,27 +41,33 @@ async function uninstall(appId) {
 document.addEventListener("DOMContentLoaded", async () => {
     const appId = sessionStorage.getItem("currentApp")
     const mainEl = document.querySelector(".app-info")
-    const appInfo = await info(appId)
-    mainEl.textContent = appInfo
 
-    const btns_container = document.querySelector(".buttons-container")
+    if (appId) {
+        const appInfo = await info(appId)
+        mainEl.textContent = appInfo
 
-    const update_btn = document.createElement("button")
-    update_btn.className = "buttons"
-    update_btn.innerText = "update"
-    btns_container.appendChild(update_btn)
-
-    const run_btn = document.createElement("button")
-    run_btn.className = "buttons"
-    run_btn.innerText = "run"
-    btns_container.appendChild(run_btn)
-
-    const uninstall_btn = document.createElement("button")
-    uninstall_btn.className = "buttons"
-    uninstall_btn.innerText = "uninstall"
-    btns_container.appendChild(uninstall_btn)
-
-    update_btn.addEventListener("click", async () => { await update(appId) })
-    run_btn.addEventListener("click", async () => { await run(appId) })
-    uninstall_btn.addEventListener("click", async () => { await uninstall(appId) })
+        const btns_container = document.querySelector(".buttons-container")
+    
+        const update_btn = document.createElement("button")
+        update_btn.className = "buttons"
+        update_btn.innerText = "update"
+        btns_container.appendChild(update_btn)
+    
+        const run_btn = document.createElement("button")
+        run_btn.className = "buttons"
+        run_btn.innerText = "run"
+        btns_container.appendChild(run_btn)
+    
+        const uninstall_btn = document.createElement("button")
+        uninstall_btn.className = "buttons"
+        uninstall_btn.innerText = "uninstall"
+        btns_container.appendChild(uninstall_btn)
+    
+        update_btn.addEventListener("click", async () => { await update(appId) })
+        run_btn.addEventListener("click", async () => { await run(appId) })
+        uninstall_btn.addEventListener("click", async () => { await uninstall(appId) })
+    }
+    else {
+        mainEl.textContent = "Choose an app!"
+    }
 })
