0081d39f61597501ee31547715833887217f8000
Camellia <camellia.sialiss@gmail.com>
2024-04-02 21:34:06 +0300

    license settings in app updater page

--- licenses/script.js
+++ licenses/script.js
@@ -1,30 +1,3 @@
 document.addEventListener("DOMContentLoaded", async () => {
-    const appId = sessionStorage.getItem("currentApp")
-    const appName = document.querySelector(".app-name")
 
-    if (appId) {
-        appName.innerText = appId
-        
-        const dateEl = document.querySelector("#expiration-date")
-        const timeEl = document.querySelector("#expiration-time")
-        const notifyEl = document.querySelector("#notify")
-
-        notifyEl.addEventListener("change", () => {
-            // notification enabled
-            if (notifyEl.checked) {
-                const dateString = `${dateEl.value} ${timeEl.value}`
-                Apps.notify(appId, dateString)
-            }
-            // notification disabled
-            else {
-                
-            }
-        })
-
-        const btns_container = document.querySelector(".buttons-container")
-        btns_container.classList.remove("hidden")
-    }
-    else {
-        appName.innerText = "Choose an app!"
-    }
 })
--- updater/script.js
+++ updater/script.js
@@ -52,23 +52,43 @@ document.addEventListener("DOMContentLoaded", async () => {
         const btns_container = document.querySelector(".buttons-container")
     
         const update_btn = document.createElement("button")
-        update_btn.className = "buttons"
-        update_btn.innerText = "update"
-        btns_container.appendChild(update_btn)
-    
         const run_btn = document.createElement("button")
-        run_btn.className = "buttons"
-        run_btn.innerText = "run"
-        btns_container.appendChild(run_btn)
-    
         const uninstall_btn = document.createElement("button")
-        uninstall_btn.className = "buttons"
+
+        update_btn.className, run_btn.className, uninstall_btn.className = "buttons"
+        
+        update_btn.innerText = "update"
+        run_btn.innerText = "run"
         uninstall_btn.innerText = "uninstall"
+
+        btns_container.appendChild(update_btn)
+        btns_container.appendChild(run_btn)
         btns_container.appendChild(uninstall_btn)
-    
+
         update_btn.addEventListener("click", async () => { await update(appId) })
         run_btn.addEventListener("click", async () => { await run(appId) })
         uninstall_btn.addEventListener("click", async () => { await uninstall(appId) })
+        
+        // notifications
+        const license_settings_container = document.querySelector("#license-settings")
+
+        const dateEl = document.querySelector("#expiration-date")
+        const timeEl = document.querySelector("#expiration-time")
+        const notifyEl = document.querySelector("#notify")
+
+        notifyEl.addEventListener("change", () => {
+            // notification enabled
+            if (notifyEl.checked) {
+                const dateString = `${dateEl.value} ${timeEl.value}`
+                Apps.notify(appId, dateString)
+            }
+            // notification disabled
+            else {
+                
+            }
+        })
+
+        license_settings_container.classList.remove("hidden")
     }
     else {
         appName.innerText = "Choose an app!"
